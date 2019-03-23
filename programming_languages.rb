7037f3393d9873f4ffc76b36a931569bbc467e8c
Gino Desangles <blushingbox@yahoo.com>
2019-03-23 11:29:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
   # your code here
+  
+  newHash = {}
+  
+  languages.each do |sty,lang|
+    lang.each do |type, x|
+      if newHash.has_key?(type)
+        newHash[type][:style] << sty
+      else
+        newHash[type] = x
+        newHash[type][:style] = [sty]
+      end
+    end
+  end
+  
+  return newHash
 end
